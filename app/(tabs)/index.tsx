c65e277e9d07ee0d8ff308047e15e61aadc5f462
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2025-02-17 08:04:58 +0530

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
   <ThemedText>
-    Fuck me daddy
+    Hello there, how are you doing
   </ThemedText>
   );
 }
